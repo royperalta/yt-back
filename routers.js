411ced0c29f338e49a71c41b61f5702c97b1aa57
royperalta <rrpb33@gmail.com>
2023-04-27 11:53:41 -0500

    update

--- routers.js
+++ routers.js
@@ -5,6 +5,8 @@ const router = express.Router()
 router.post('/download', (req, res) => {
    const body = req.body
    console.log(body)
+   const command = `yt-dlp -i -x --audio-format mp3 ${body.url}` 
+   res.send("OK")
 })
 
 
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { router } from './routers.js'
+import cors from 'cors'
 const app = express()
 
 dotenv.config({path:'.env'})
 
 app.use(express.json())
+app.use(cors())
 app.use('/api',router)
 
 const PORT = process.env.PORT || 20000
